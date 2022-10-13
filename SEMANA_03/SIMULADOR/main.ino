f569eeb596e566f6f3c71675d2727fc0fae17cd5
ViniciosLugli <vinicioslugli@gmail.com>
2022-10-13 13:45:08 -0300

    Update notes in S3/SIM

--- SEMANA_03/SIMULADOR/main.ino
+++ SEMANA_03/SIMULADOR/main.ino
@@ -1,15 +1,8 @@
 
 /*
-Construa um programa que seja capaz de ler os dados de luz de
-um sensor LDR, converter o valor lido em úm número de 0 a 15 e use esse valor para acender 4 LEDs, cada um para um dígito binário que irá corresponder
-a conversão do número (0 a 15). Além disso:
-1 - Faça uma função que, a cada novo número medido, faça a contagem binária nos leds, pausadamente, de 0
-até o número medido.
-2 - Acrescente "toque" usando um Buzzer que corresponda a uma nota diferente para cada número. Ao contar de 0 até o número, o
-buzzer irá tocando todas as notas correspondentes.
-3 - Acrescente um "push button", tal que, ao ser pressionado, armazene o valor medido em um vetor.
-4 - Acrescente um SEGUNDO push button que, ao ser pressionado, toque todos os tons correspondentes ao número armazenado.
-5 - Ponto de desafio: Acrescente um display ao seu sistema que vá indicando as medidas que vao ocorrendo, assim como as notas tocadas
+Notas de uso:
+Botão azul ( porta 26 ) faz o salvamento do valor no vetor após a leitura do sensor ( leds binários )
+Botão verde ( porta 25 ) reproduz os sons dos valores no final da leitura do sensor ( leds binários )
 */
 
 #include <Adafruit_SSD1306.h>
